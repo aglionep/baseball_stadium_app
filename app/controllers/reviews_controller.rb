5cf2bedc7e7b60137e2818e0829a2f237288ee87
Paul Aglione <aglionep@gmail.com>
2015-06-02 16:44:55 -0700

    last commit before branching with postgres then installed postgres

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,25 @@
 class ReviewsController < ApplicationController
 
+	# def index
+	# 	@review = Review.all
+	# end
+	
 	def new
 		@review = Review.new
 	end
 
 	def create
 		@review = Review.new(review_params)
+		if @review.save
 			redirect_to stadiums_path
+		else
+			render :new
+		end
 	end
+
+	def edit
+		@review = Review.find(params[:id]) 
+    end
 
 end
 
